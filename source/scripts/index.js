7d6ed6f54d063aaf9051723e9f739f2963d198fc
lizzeppelin758 <lizzeppelin758@gmail.com>
2024-10-18 21:21:00 +0600

    промежуточные результаты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
 /* в этот файл добавляет скрипты*/
+
+/*burger menu*/
+
+const menuButton = document.querySelector('.js-toggle-button');
+const menu = document.querySelector('.navigation__menu');
+
+menuButton.addEventListener('click', ()=>{
+  menu.classList.toggle('navigation__menu--opened');
+  menuButton.classList.toggle('js-toggle-button--close');
+});
